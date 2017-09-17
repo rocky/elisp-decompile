199a14246423c8fde62dc8fb4a087e5da8540fd0
rocky <rb@dustyfeet.com>
2017-09-17 10:48:39 -0400

    More tests

---
+++ testdata/test-list.el
@@ -0,0 +1,10 @@
+(defun test-list(e)
+  (format "%s %s"
+	  (list 1 2 3 4 5)
+	  (concat a b c d e f g)))
+
+
+(defun test-list-small(a b c d e f g)
+  (format "%s %s"
+	  (list 1 2 3 4)
+	  (concat a b c d)))
