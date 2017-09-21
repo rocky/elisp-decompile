9435991f48aad3f0a4ec5aab13334e58639830ad
rocky <rb@dustyfeet.com>
2017-09-20 20:16:08 -0400

    Fix indentation on "let"

--- eldecompile/semantics.py
+++ eldecompile/semantics.py
@@ -1,3 +1,77 @@
+# The below is a bit long, but still it is somehwat abbreviated.
+# See https://github.com/rocky/python-uncompyle6/wiki/Table-driven-semantic-actions.
+# for a more complete explanation, nicely marked up and with examples.
+#
+#
+# Semantic action rules for nonterminal symbols can be specified here by
+# creating a method prefaced with "n_" for that nonterminal. For
+# example, "n_exec_stmt" handles the semantic actions for the
+# "exec_stmt" nonterminal symbol. Similarly if a method with the name
+# of the nonterminal is suffixed with "_exit" it will be called after
+# all of its children are called.
+#
+# However if this were done for all of the rules, this file would be even longer
+# than it is already.
+#
+# Another more compact way to specify a semantic rule for a nonterminal is via
+# rule given in one of the tables MAP_R0, MAP_R, or MAP_DIRECT.
+#
+# These uses a printf-like syntax to direct substitution from attributes
+# of the nonterminal and its children..
+#
+# The rest of the below describes how table-driven semantic actions work
+# and gives a list of the format specifiers. The default() and
+# template_engine() methods implement most of the below.
+#
+#   Step 1 determines a table (T) and a path to a
+#   table key (K) from the node type (N) (other nodes are shown as O):
+#
+#          N&K               N                  N
+#         / | ... \        / | ... \          / | ... \
+#        O  O      O      O  O      K         O O      O
+#                                                      |
+#                                                      K
+#      TABLE_DIRECT      TABLE_R             TABLE_R0
+#
+#   The default is a "TABLE_DIRECT" mapping.  The key K is then extracted from the
+#   subtree and used to find a table entry T[K], if any.  The result is a
+#   format string and arguments (a la printf()) for the formatting engine.
+#   Escapes in the format string are:
+#
+#     %c  evaluate the node recursively. Its argument is a single
+#         integer representing a node index.
+#
+#     %Q  like %c but assumes the first argument has already been quoted.
+#         Used in things like (setq x ...) where x might be a variable ref
+#         which shouldn't be quoted. Calls are simlar too.
+#
+#     %l  takes a tuple of node indices: low and high. Evaluates
+#         nodes low to high inserting a space in between each one
+#
+#     %S  pops a value of the eval stack to use as an argument.
+#         _stacked nonterminals work this way
+#
+#     %|  Insert spaces to the current indentation level. Takes no arguments.
+#
+#     %. Set the indent level to be where we currently are at[d. Takes no arguments.
+
+#     %+ increase current indentation level. Takes no arguments.
+#        pushes indent level on onto stack
+#
+#     %- decrease current indentation level. Takes no arguments.
+#        indent level is obtained from the stack
+#
+#     %{...} evaluate ... in context of N
+#
+#     %% literal '%'. Takes no arguments.
+#
+#
+#   The '%' may optionally be followed by a number (C) in square
+#   brackets, which makes the template_engine walk down to N[C] before
+#   evaluating the escape code.
+
+from __future__ import print_function
+
 import re, sys
 from spark_parser import GenericASTTraversal
 
@@ -34,10 +108,10 @@ TABLE_DIRECT = {
     'if_expr':		( '%(if %c\n%+%|%c%)', 0, 2 ),
     'if_else_expr':	( '%(if %c\n%+%|%c%_%c%)%_', 0, 2, 6 ),
 
-    'let_expr_stacked':	( '%(let (%.%c)%-%c%)', 0, 1 ),
-    'let_expr_star':	( '%(let* (%c)%-%c%)', 0, 1 ),
+    'let_expr_stacked':	( '%(let %.(%.%c)%-%-%c%)', 0, 1 ),
+    'let_expr_star':	( '%(let* %.(%.%c)%-%-%c%)', 0, 1 ),
     'progn':		( '%(progn%+%c)', 0 ),
-    'body_stacked':	( '%+%c%-', 0 ),
+    'body_stacked':	( '%c', 0 ),
     'expr':		( '%C', (0, 10000) ),
     'expr_stacked':	( '%C', (0, 10000) ),
 
@@ -128,15 +202,17 @@ class SourceWalker(GenericASTTraversal, object):
                  lambda s: s.params.__delitem__('indent'),
                  None)
 
-    def __init__(self, ast):
+    def __init__(self, ast, debug=True):
         GenericASTTraversal.__init__(self, ast=None)
         params = {
             'indent': '',
             }
         self.params = params
+        self.debug = debug
         self.ERROR = None
         self.pending_newlines = 0
         self.hide_internal = True
+        self.indent_stack = ['']
 
         # A place to put the AST nodes for compuations pushed
         # on the evaluation stack
@@ -196,11 +272,20 @@ class SourceWalker(GenericASTTraversal, object):
             self.f.write(u"'")
         self.f.write(to_s(node.attr))
 
-    def indentMore(self, indent=TAB):
+    def indent_more(self, indent=TAB):
         self.indent += indent
+        if self.debug:
+            print("XXX indent more len %d" % len(self.indent))
+        self.indent_stack.append(self.indent)
 
-    def indentLess(self, indent=TAB):
-        self.indent = self.indent[:-len(indent)]
+    def indent_less(self, indent=None):
+        if indent is None:
+            self.indent = self.indent_stack.pop()
+        else:
+            self.indent_stack[-1] = self.indent
+            self.indent = self.indent[:-len(indent)]
+        if self.debug:
+            print("XXX indent less len %d" % len(self.indent))
 
     def traverse(self, node, indent=None, isLambda=False):
         if indent is None: indent = self.indent
@@ -244,7 +329,7 @@ class SourceWalker(GenericASTTraversal, object):
     def template_engine(self, entry, startnode):
         """The format template engine.  See the comment at the beginning of
         this module for the how we interpret format specifications
-        such as %c, %C, %l and so on.
+        such as %c, %C, %l and so on and what arguments they expect.
         """
 
         # self.println("-----")
@@ -271,10 +356,12 @@ class SourceWalker(GenericASTTraversal, object):
             if   typ == '%':	self.write('%')
             elif typ == '.':
                 count = len(self.f.getvalue().split("\n")[-1]) - len(self.indent)
-                self.indentMore(' ' * count)
-            elif typ == '+':	self.indentMore()
-            elif typ == '-':	self.indentLess()
-            elif typ == '_':	self.indentLess('  ')  # For else part of if/else
+                if self.debug:
+                    print("XXX . indent count", count)
+                self.indent_more(' ' * count)
+            elif typ == '+':	self.indent_more()
+            elif typ == '-':	self.indent_less()
+            elif typ == '_':	self.indent_less('  ')  # For else part of if/else
             elif typ == '|':	self.write(self.indent)
             elif typ == 'c':
                 self.preorder(node[entry[arg]])
@@ -314,18 +401,6 @@ class SourceWalker(GenericASTTraversal, object):
                         pass
                     pass
                 arg += 1
-            elif typ == 'P':
-                p = self.prec
-                low, high, sep, self.prec = entry[arg]
-                remaining = len(node[low:high])
-                # remaining = len(node[low:high])
-                for subnode in node[low:high]:
-                    self.preorder(subnode)
-                    remaining -= 1
-                    if remaining > 0:
-                        self.write(sep)
-                self.prec = p
-                arg += 1
             elif typ == '{':
                 d = node.__dict__
                 expr = m.group('expr')
@@ -340,7 +415,7 @@ class SourceWalker(GenericASTTraversal, object):
                 self.write('(')
             elif typ == ')':
                 self.write(')')
-                self.indentLess()
+                self.indent_less()
             m = escape.search(fmt, i)
         self.write(fmt[i:])
 
